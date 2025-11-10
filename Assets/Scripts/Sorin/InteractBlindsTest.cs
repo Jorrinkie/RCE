5ab7e88a823640992b3044957437753369e6aabd
Sor1nel <sofiansorin1003@gmail.com>
2025-11-10 23:33:23 +0100

    package imported

--- Assets/Scripts/Sorin/InteractBlindsTest.cs
+++ Assets/Scripts/Sorin/InteractBlindsTest.cs
@@ -16,7 +16,7 @@ public class InteractBlindsTest : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if (Input.GetKeyDown(KeyCode.E))
         {
             if (outlineChecker != null && outlineChecker.IsLookingAtHeritage)
             {
